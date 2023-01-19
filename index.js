6e3cfcb5458b837138aee0c987993457f9717a36
Lubomír Starý <lubomir.stary@ixperta.com>
2023-01-19 16:47:32 +0100

    fixed typo from version 0.2.6

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ function createLightSummary(rawDetail, options) {
       if (CTr && CTr.includes('application/json'))
         response.body = convertToJson(response.body);
       if (CTr && CTr.includes('image/png'))
-        reponse.body = '<<< truncated as image/png content-type >>>';
+        response.body = '<<< truncated as image/png content-type >>>';
       Object.assign(step, {
         'response': response,
       });
