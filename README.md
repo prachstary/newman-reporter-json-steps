96dd2dcad733d6420c2990f8fec914a2f6243f5c
soamicharan <charansoami@gmail.com>
2023-07-05 16:49:46 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-# newman-reporter-json-steps
+# newman-reporter-all
 
-JSON Reporter focused on executed steps in Newman report. Every step contains request, response and test states.
-
+Newman reporter for multiple format summary generation.
 ## Motivation
 
 Every json reporters have always some exceptions and incomplete informations. Also written in vanilla node.js compatible with v8 to be easily installed anywhere.
 
 Thanks for inspiration in many other json exports.
 
-## Output structure
+## json-steps Output structure
 
 ```json
 {
@@ -76,13 +75,69 @@ Thanks for inspiration in many other json exports.
 }
 ```
 
+## xray-json Output Structure
+
+```
+{
+  "testExecutionKey": "WRFL-123",
+  "info": {
+    "project": "WRFL",
+    "summary": "Execution of automated tests",
+    "description": "This execution is automatically created when importing execution results from Gitlab",
+    "version": "",
+    "revision": "",
+    "startDate": "2023-07-05T06:18:04.815-05:30",
+    "finishDate": "2023-07-05T06:18:10.603-05:30",
+    "testPlanKey": "",
+    "testEnvironments": []
+  },
+  "tests": [
+    {
+      "testKey": "WRFL-144",
+      "comments": "[WRFL-144] Step 3: Retrieving Workflow Modules with no matching filters - Assertion - Status code is 200 FAILED. Detail - expected response to have status code 201 but got 200\n[WRFL-144] Step 3: Retrieving Workflow Modules with no matching filters - Assertion - Should contain not contain any Workflow modules data FAILED. Detail - expected 0 to equal 1",
+      "status": "FAILED",
+      "start": "2023-07-05T06:18:10.606-05:30",
+      "finish": "2023-07-05T06:18:10.607-05:30",
+      "executedBy": "",
+      "assignee": "",
+      "defects": [],
+      "evidence": [],
+      "customFields": []
+    }
+  ]
+}
+```
+
 [Structures described.](https://github.com/postmanlabs/newman#newmanruncallbackerror-object--summary-object)
 
 ## Options
 
-Option | Value | Optional | Note
--- | -- | --
---reporter-json-steps-export | <path/to/generate/json/report> | Yes |
---reporter-json-steps-stats  | | Yes | Include also stats into report
+| Option                                 | Value                        | Required | Note                                                                                |
+|----------------------------------------|------------------------------|----------|-------------------------------------------------------------------------------------|
+| --reporter-all-export                  | /path/to/save/json/file.json | YES      | Path to dump report                                                                 |
+| --reporter-all-exporter-engine         | <reporter engine type>       | NO       | Allowed values are `xray-json`, `json-steps`. By default `json-steps` will be used. |
+| --reporter-all-xray-project-key        | <JIRA Project Key>           | YES      | This option is required for `xray-json` exporter. Sample value `WRFL`               |
+| --reporter-all-xray-test-execution-key | <JIRA Test Execution Ticket> | YES      | This option is required for `xray-json` exporter. Sample value `WRFL-123`           |
+| --reporter-all-json-steps-stats        | No Value required            | No       | This option used for `json-steps` exporter. This includes test stats in report.     |
 
 *By default the report is generated in `newman` subfolder of current working directory.*
+
+## Usage
+
+### json-steps Usage
+```bash
+newman run https://www.getpostman.com/collections/631643-f695cab7-6878-eb55-7943-ad88e1ccfd65-JsLv \
+-r 'all,cli' \
+--reporter-all-export './examples/result.json' \
+--reporter-all-json-steps-stats
+```
+
+### xrya-json Usage
+```bash
+newman run 'https://www.getpostman.com/collections/631643-f695cab7-6878-eb55-7943-ad88e1ccfd65-JsLv' \
+-r 'all,cli' \
+--reporter-all-export ./test.json \
+--reporter-all-exporter-engine xray-json \
+--reporter-all-xray-project-key WRFL \
+--reporter-all-xray-test-execution-key WRFL-124
+```
--- index.js
+++ index.js
@@ -21,6 +21,9 @@ function error(...msg) {
 }
 
 function scrapeDescription(x) {
+  if (x == undefined) {
+    return undefined;
+  }
   var ret = (x.type === 'text/plain') ? x.content : undefined;
   return ret;
 }
@@ -41,6 +44,16 @@ function compactHeaders(allHeaders) {
     h[x.key] = x.value;
   });
   return h;
+}
+
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+  const offset = date.getTimezoneOffset() * -1;
+  const offsetHours = Math.floor(Math.abs(offset) / 60).toString().padStart(2, '0');
+  const offsetMinutes = (Math.abs(offset) % 60).toString().padStart(2, '0');
+  const offsetSign = offset >= 0 ? '-' : '+';
+
+  return `${date.toISOString().slice(0, -1)}${offsetSign}${offsetHours}:${offsetMinutes}`;
 }
 
 function createLightSummary(rawDetail, options) {
@@ -118,6 +131,97 @@ function createLightSummary(rawDetail, options) {
   return ret;
 }
 
+function createXraySummary(rawDetail, options){
+  if (options.xrayProjectKey == undefined) {
+    throw new Error('Project Key is not provided')
+  }
+
+  if (options.xrayTestExecutionKey == undefined) {
+    throw new Error('Test Execution Key is not Provided.')
+  }
+
+  let steps = [];
+  let test_steps = {};
+  rawDetail.run.executions.forEach(function (exec) {
+    let test_status = 'PASSED';
+    let test_failed_details = [];
+    if (exec.assertions !== undefined) {
+      exec.assertions.forEach(function (assertionReport) {
+        if (assertionReport.error) {
+          test_status = 'FAILED';
+          test_failed_details.push(`${exec.item.name} - Assertion - ${assertionReport.assertion} FAILED. Detail - ${assertionReport.error.message}`);
+        }
+      });
+    }
+    if (exec.requestError) {
+      test_status = 'FAILED';
+      test_failed_details.push(`${exec.item.name} Request Error - ${exec.requestError}`);
+    }
+    let test_comments = (test_failed_details.length > 0) ? test_failed_details.join('\n') : '';
+
+    let test_key = exec.item.name.match(/\[(.+?)\]/);
+    if (test_key == undefined || test_key == null || test_key.length < 2) {
+      throw new Error('Test key tag not found');
+    }
+
+    test_key = test_key[1];
+    if (test_key.startsWith(options.xrayProjectKey) == false) {
+      throw new Error('Test key is invalid.')
+    }
+    if (test_steps[test_key] == undefined) {
+      test_steps[test_key] = {
+        'status': 'PASSED',
+        'comments': [],
+        'response_time': 0
+      }
+    }
+    if (test_comments != '') {
+      test_steps[test_key].comments.push(test_comments)
+    }
+    if (test_steps[test_key].status == 'FAILED' || test_status == 'FAILED') {
+      test_steps[test_key].status = 'FAILED';
+    }
+    if (exec.response.responseTime) {
+      test_steps[test_key].response_time += (exec.response.responseTime / 1000) || 0;
+    }
+  });
+
+  Object.keys(test_steps).forEach(function (step) {
+    var currentTimestamp = Date.now();
+    steps.push({
+      'testKey': step,
+      'comments': test_steps[step].comments.join('\n'),
+      'status': test_steps[step].status,
+      'start': `${formatTimestamp(currentTimestamp)}`,
+      'finish': `${formatTimestamp(currentTimestamp + Math.round(test_steps[step].response_time))}`,
+      "executedBy": "",
+      "assignee": "",
+      "defects": [],
+      "evidence": [],
+      "customFields": [],
+    })
+  })
+
+  let xray_json = {
+    "testExecutionKey": options.xrayTestExecutionKey,
+    'info': {
+      'project': options.xrayProjectKey,
+      "summary": "Execution of automated tests",
+      "description": "This execution is automatically created when importing execution results from Gitlab",
+      "version": "",
+      "revision": "",
+      "startDate": `${formatTimestamp(rawDetail.run.timings.started)}`,
+      "finishDate": `${formatTimestamp(rawDetail.run.timings.completed)}`,
+      "testPlanKey": "",
+      "testEnvironments": []
+    },
+    'tests': steps,
+  }
+
+  return xray_json;
+}
+
+
 module.exports = function (newman, options) {
   newman.on('beforeDone', function (err, o) {
     // info(options);
@@ -125,12 +229,24 @@ module.exports = function (newman, options) {
       info('stops on error:', err);
       return;
     }
+    
+    let exportPath = options.export;
+    if (exportPath == undefined) {
+      throw new Error('Provide result export path.')
+    }
+
+    report_generator_function = createLightSummary
+    let exporterName = options.exporterEngine;
+    if (exporterName == 'xray-json'){
+      report_generator_function = createXraySummary
+    }
+
     try {
       newman.exports.push({
-        name: 'json-steps-reporter',
+        name: 'all-reporter',
         default: 'newman-step-results.json',
-        path: options.jsonStepsExport,
-        content: createLightSummary(o.summary, options)
+        path: exportPath,
+        content: report_generator_function(o.summary, options)
       });
     }
     catch (e) {
